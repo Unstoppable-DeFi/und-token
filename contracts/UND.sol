582a35c3a957ac59516abc29e1c205bc5a818584
Unstoppable-DeFi <119927293+Unstoppable-DeFi@users.noreply.github.com>
2023-03-26 07:50:41 +0000

    add ERC20Burnable

--- contracts/UND.sol
+++ contracts/UND.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact team@unstoppable.ooo
-contract UnstoppableEcosystemToken is ERC20 {
+contract UnstoppableEcosystemToken is ERC20, ERC20Burnable {
     constructor() ERC20("Unstoppable Ecosystem Token", "UND") {
         _mint(msg.sender, 100_000_000 * 10 ** decimals());
     }
